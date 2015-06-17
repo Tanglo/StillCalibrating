8db0b5f4ef56f4ade7f33f2a7f1ba945f81fb959
Tanglo <Tanglo@users.noreply.github.com>
2015-06-17 17:29:31 +1000

    Captured images are consistent with video preview
     - Also added flip check boxes

--- Still Calibrating/SCPreviewViewController.swift
+++ Still Calibrating/SCPreviewViewController.swift
@@ -15,6 +15,8 @@ class SCPreviewViewController: NSViewController {
     var videoDevices = [AVCaptureDevice]()
     var selectedDevice: AVCaptureDevice?
     var session = AVCaptureSession()
+    var flipHorizontally = false
+    var flipVertically = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,34 +30,48 @@ class SCPreviewViewController: NSViewController {
             }
         }
         cameraPopup!.selectItem(nil)
+        self.addObserver(self, forKeyPath: "flipHorizontally", options: .allZeros, context: nil)
+        self.addObserver(self, forKeyPath: "flipVertically", options: .allZeros, context: nil)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        selectCamera(cameraPopup!)
     }
     
     @IBAction func selectCamera(sender: AnyObject){
-        if !self.view.wantsLayer {
-            self.view.wantsLayer = true
-        } else {
-            for layer in self.view.layer!.sublayers{
-                layer.removeFromSuperlayer()
+        if cameraPopup!.selectedItem != nil {
+            if !self.view.wantsLayer {
+                self.view.wantsLayer = true
+            } else {
+                for layer in self.view.layer!.sublayers{
+                    layer.removeFromSuperlayer()
+                }
             }
+            session.sessionPreset = AVCaptureSessionPresetMedium
+            let captureVideoPreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(session) as? AVCaptureVideoPreviewLayer
+            captureVideoPreviewLayer!.frame = self.view.bounds
+            self.view.layer!.addSublayer(captureVideoPreviewLayer)
+            captureVideoPreviewLayer!.autoresizingMask = CAAutoresizingMask.LayerWidthSizable | CAAutoresizingMask.LayerHeightSizable
+            captureVideoPreviewLayer!.transform = CATransform3DIdentity
+            if flipHorizontally {
+                captureVideoPreviewLayer!.transform = CATransform3DConcat(captureVideoPreviewLayer!.transform, CATransform3DMakeRotation(CGFloat(M_PI), 0.0, 1.0, 0.0))
+            }
+            if flipVertically {
+                captureVideoPreviewLayer!.transform = CATransform3DConcat(captureVideoPreviewLayer!.transform, CATransform3DMakeRotation(CGFloat(M_PI), 1.0, 0.0, 0.0))
+            }
+            
+            selectedDevice = videoDevices[sender.indexOfSelectedItem()]
+            var error: NSError?
+            let deviceInput = AVCaptureDeviceInput.deviceInputWithDevice(selectedDevice, error: &error) as? AVCaptureDeviceInput
+            if deviceInput == nil {
+                let errorAlert = NSAlert(error: error!)
+                errorAlert.runModal()
+            }
+            session.addInput(deviceInput)
+            let stillOutput = AVCaptureStillImageOutput()
+            session.addOutput(stillOutput)
+            session.startRunning()
         }
-        session.sessionPreset = AVCaptureSessionPresetMedium
-        let captureVideoPreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(session) as? AVCaptureVideoPreviewLayer
-        captureVideoPreviewLayer!.frame = self.view.bounds
-        self.view.layer!.addSublayer(captureVideoPreviewLayer)
-        captureVideoPreviewLayer!.autoresizingMask = CAAutoresizingMask.LayerWidthSizable | CAAutoresizingMask.LayerHeightSizable
-        captureVideoPreviewLayer!.transform = CATransform3DMakeRotation(CGFloat(M_PI), 0.0, 1.0, 0.0)
-        
-        selectedDevice = videoDevices[sender.indexOfSelectedItem()]
-        var error: NSError?
-        let deviceInput = AVCaptureDeviceInput.deviceInputWithDevice(selectedDevice, error: &error) as? AVCaptureDeviceInput
-        if deviceInput == nil {
-            let errorAlert = NSAlert(error: error!)
-            errorAlert.runModal()
-        }
-        session.addInput(deviceInput)
-        let stillOutput = AVCaptureStillImageOutput()
-        session.addOutput(stillOutput)
-        session.startRunning()
     }
     
     @IBAction func captureStillImage(sender: AnyObject){
@@ -76,8 +92,11 @@ class SCPreviewViewController: NSViewController {
                     if (exifAttachments != nil) {
                         //use attachments
                     }
-                    let capturedImageRep = NSBitmapImageRep(data: AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer))
-                    let pngData = capturedImageRep?.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [NSObject: AnyObject]())
+                    var capturedImageRep = NSBitmapImageRep(data: AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer))
+                    if self.flipHorizontally || self.flipVertically{
+                        capturedImageRep = self.flipBitmapImageRep(capturedImageRep!)
+                    }
+                    var pngData = capturedImageRep?.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [NSObject: AnyObject]())
                     if pngData != nil {
                         pngData!.writeToFile(filePath, atomically: true)
                     }
@@ -86,6 +105,25 @@ class SCPreviewViewController: NSViewController {
         } else {
             NSBeep()
         }
+    }
+    
+    func flipBitmapImageRep(bitmapImageRep: NSBitmapImageRep) -> NSBitmapImageRep{
+        let flippedImage = NSImage(size: bitmapImageRep.size)
+        flippedImage.lockFocus()
+        var transform = NSAffineTransform()
+        if flipHorizontally{
+            transform.scaleXBy(-1.0, yBy: 1.0)
+            transform.translateXBy(-flippedImage.size.width, yBy: 0.0)
+        }
+        if flipVertically{
+            transform.scaleXBy(1.0, yBy: -1.0)
+            transform.translateXBy(0.0, yBy: -flippedImage.size.height)
+        }
+        transform.concat()
+        bitmapImageRep.drawInRect(NSRect(origin: NSPoint(x: 0.0, y: 0.0), size: flippedImage.size))
+        flippedImage.unlockFocus()
+        let flippedData = flippedImage.TIFFRepresentation
+        return NSBitmapImageRep(data: flippedData!)!
     }
     
 }
